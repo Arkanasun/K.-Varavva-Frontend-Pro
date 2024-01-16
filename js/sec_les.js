c6555c2f7e2a9901aa130b15d97316268bd5ea15
Arkanasun <arkana.sun@gmail.com>
2024-01-16 22:14:44 +0200

    4th commit

---
+++ js/sec_les.js
@@ -0,0 +1,23 @@
+do{
+  num1 = +prompt('Введите первое число', "")
+    num2 = +prompt('Введите второе число', "")
+}while (isNaN(num1) || isNaN(num2))
+alert(num1+num2)
+
+do{
+  num1 = +prompt('Введите первое число', "")
+  num2 = +prompt('Введите второе число', "")
+}while (isNaN(num1) || isNaN(num2))
+alert(num1-num2)
+
+do{
+  num1 = +prompt('Введите первое число', "")
+  num2 = +prompt('Введите второе число', "")
+}while (isNaN(num1) || isNaN(num2))
+alert(num1*num2)
+
+do{
+  num1 = +prompt('Введите первое число', "")
+  num2 = +prompt('Введите второе число', "")
+}while (isNaN(num1) || isNaN(num2))
+alert(num1/num2)
