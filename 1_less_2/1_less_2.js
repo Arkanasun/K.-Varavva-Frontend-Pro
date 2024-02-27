164e23f4b5584970abac17a4e0edfb1f885632a9
Arkanasun <arkana.sun@gmail.com>
2024-02-27 19:25:53 +0200

    1_lesson

---
+++ 1_less_2/1_less_2.js
@@ -0,0 +1,4 @@
+const ours = +prompt(`Введите количество часов`)
+alert(ours*60)
+
+s
--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
-let res = prompt('What is Your name?')
-alert('Hello,'+res+'! How are you?')
+const num1 = prompt(`Введите первое число`);
+const num2 = prompt(`Введите второе число`);
+const operator = prompt(`Выберите необходимое действие:  div,add,multi,sub`);
+if(operator === "add") {
+ alert(num1+'+'+num2+'='+(num1 + num2));
+}
+if(operator === "sub") {
+ alert(num1+'-'+num2+'='+(num1 - num2));
+}
+if(operator === "multi") {
+ alert(num1+'*'+num2+'='+(num1 * num2));
+}
+if(operator === "div") {
+ alert(num1+'/'+num2+'='+(num1 / num2));
+}
