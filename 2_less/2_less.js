802f86eb3ae92fd8192c4f05df1a412258b65a9a
Arkanasun <arkana.sun@gmail.com>
2024-02-28 22:22:10 +0200

    fixed_first_hometask

---
+++ 2_less/2_less.js
@@ -0,0 +1,8 @@
+const age = +prompt(`Введите год своего рождения`);
+alert(2024 - age);
+const city = prompt(`В каком городе Вы живёте?`)
+switch (city){
+    case `Киев`:
+        alert("Вы живёте в столице Украины");
+        break
+}
--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 const num1 = prompt(`Введите первое число`);
 const num2 = prompt(`Введите второе число`);
-const operator = prompt(`Выберите необходимое действие:  div,add,multi,sub`);
-if(operator === "add") {
- alert(num1+'+'+num2+'='+(num1 + num2));
+const operator = prompt(`Выберите необходимое действие:  /,*,+,-`);
+if(operator === "+") {
+ alert(`${num1} + ${num2} = ${+num1 + +num2}`);
 }
-if(operator === "sub") {
- alert(num1+'-'+num2+'='+(num1 - num2));
+if(operator === "-") {
+ alert(`${num1} - ${num2} = ${num1 - num2}`);
 }
-if(operator === "multi") {
- alert(num1+'*'+num2+'='+(num1 * num2));
+if(operator === "*") {
+ alert(`${num1} * ${num2} = ${num1 * num2}`);
 }
-if(operator === "div") {
- alert(num1+'/'+num2+'='+(num1 / num2));
+if(operator === "/") {
+ alert(`${num1} / ${num2} = ${num1 / num2}`);
 }
